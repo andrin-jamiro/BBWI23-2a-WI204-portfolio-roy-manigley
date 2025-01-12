9c20928ca0f62e372b827cf67b8bedf6ce03c9ec
Andrin Ambühl <andrin.ambuehl@hftm.ch>
2025-01-12 17:24:19 +0100

    modified

--- src/app/components/cv/cv.component.spec.ts
+++ src/app/components/cv/cv.component.spec.ts
@@ -18,27 +18,17 @@ describe('CVComponent', () => {
         {
           provide: ActivatedRoute,
           useValue: {
-<<<<<<< HEAD
-            data: of({'records': [
-              new CV('Alpha', 1, 1),
-              new CV('Bravo', 1, 2),
-              new CV('Charlie', 1, 3),
-              new CV('Delta', 1, 4),
-              new CV('Echo', 1, 5),
-            ]}),
-=======
             data: of(
               {
                 'records': [
-                  new CV('Alpha', 1, 1), 
-                  new CV('Bravo', 1, 2), 
-                  new CV('Charlie', 1, 3), 
-                  new CV('Delta', 1, 4), 
+                  new CV('Alpha', 1, 1),
+                  new CV('Bravo', 1, 2),
+                  new CV('Charlie', 1, 3),
+                  new CV('Delta', 1, 4),
                   new CV('Echo', 1, 5),
                 ]
               }
           ),
->>>>>>> 8f4937d944945e2ba22f5a506f5d6d6c581f7de1
           },
         }
       ]
@@ -59,11 +49,7 @@ describe('CVComponent', () => {
     expect(component.cvs.length).toBe(5);
   });
 
-<<<<<<< HEAD
-  it('it should sort the cvs by end desc', () => {
-=======
   it('should sort the CVs by end DESC', () => {
->>>>>>> 8f4937d944945e2ba22f5a506f5d6d6c581f7de1
     component.loadCVs()
     expect(component.cvs[0].employer).toBe('Echo');
     expect(component.cvs[1].employer).toBe('Delta');
